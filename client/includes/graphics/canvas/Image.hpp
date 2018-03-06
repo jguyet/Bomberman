8e7ce1eb49bb7bd533921f1db623a7f93710c6e8
Jeremy GUYET <jguyet@e1r11p3.42.fr>
2018-03-06 05:18:58 +0100

    Resize image performances, lock if fullscreen is actived button, remove simple loading of image

--- client/includes/graphics/canvas/Image.hpp
+++ client/includes/graphics/canvas/Image.hpp
@@ -9,31 +9,38 @@ class Image : public Tag
 		// STATICS ############################################################
 		// ####################################################################
 		// CANONICAL ##########################################################
-										Image( const char *path );
-										Image (const char *path, int base_width, int base_height, int width, int height);
+										Image (std::string const &path, int base_width, int base_height, int width, int height);
 										Image( Image const & src );
 		virtual							~Image( void );
 		Image &							operator=( Image const & rhs );
 		friend std::ostream &			operator<<(std::ostream & o, Image const & i);
 		// ####################################################################
 		// PUBLICS ############################################################
-		void								setFloat(e_tag_position position);
-		void								setFontFamily(std::string const &fontname);
-		void								setFontSize(int font_size);
-		void								setColor(glm::vec3 &color);
-		void								setTextAlign(e_tag_position position);
-		void								setBackgroundColor(glm::vec3 &color);
-		void								setBackgroundImage(std::string const &path);
-		void								setDisplay(bool visible);
-		void								setStyle(std::string const &style);
-		void								draw(SDL_Surface *surface);
-		bool								equals(Tag *tag);
+		void							load_image(void);
+		void							resize_image(void);
+
+		void							setFloat(e_tag_position position);
+		void							setFontFamily(std::string const &fontname);
+		void							setFontSize(int font_size);
+		void							setColor(glm::vec3 &color);
+		void							setTextAlign(e_tag_position position);
+		void							setBackgroundColor(glm::vec3 &color);
+		void							setBackgroundImage(std::string const &path);
+		void							setDisplay(bool visible);
+		void							setStyle(std::string const &style);
+		void							draw(SDL_Surface *surface);
+		bool							equals(Tag *tag);
 		// ####################################################################
 	private:
 		// PRIVATES ###########################################################
 		SDL_Surface						*image;
-		int								width = -1;
-		int								height = -1;
+		std::string						path;
+		int								width = 0;
+		int								height = 0;
+		bool							loaded = false;
+		bool							resized = true;
+		int								base_width = 0;
+		int								base_height = 0;
 		// ####################################################################
 };
 
